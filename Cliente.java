ddd5b28d4814347c18e3b032e2aed809c52d8ce3
Lyssa_ramoss <melyssaramos98@gmail.com>
2023-04-04 14:29:03 +0000

    Encapsulamento Conta

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+public class CLiente{
+
+    public static  void main(String[]args){
+
+        Conta conta=new Conta();
+        conta.nome="Mell";
+        conta.salario=10000;
+              
+            System.out.println(conta.nome);
+            System.out.println(conta.salario);
+
+    }
+}
---
+++ Conta.java
@@ -0,0 +1,37 @@
+public class Conta{
+
+    private int numero;
+    double salario;
+    protected String nome;
+
+    numero=382;
+
+    public void setNumero(numero:392);
+    {
+        this.numero = numero;
+
+    }
+    public int getNumero(){
+        return this.numero;
+    }
+     public void setSalario(int salario){
+        this.salario = salario;
+
+    }
+    public int getSalario(){
+        return this.salario;
+    }
+     public void setNome(int nome){
+        this.nome = nome;
+
+    }
+    public int getNome(){
+        return this.nome;
+    }
+    
+    
+
+
+
+
+}
